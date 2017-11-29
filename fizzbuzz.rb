7bcdaa7349636587152ef1c96f9b82631df603c9
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-29 01:42:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@ def fizzbuzz(n)
   if n % 3 == 0
     "Fizz"
   end
-  elsif n % 5 == 0
+  if n % 5 == 0
     "Buzz"
   end
   binding.pry
-  elsif n % 3 == 0 && n % 5 == 0
+  if n % 3 == 0 && n % 5 == 0
     "FizzBuzz"
   end
 end
